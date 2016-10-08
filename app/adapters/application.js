d4b7cd3acea452093495bedd737a65b9654e0711
serloap <sergio0195@hotmail.com>
2016-10-08 03:01:14 -0500

    ultimo commit, eso no dió, me pondré en posición fetal

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-  host: 'https://restaurant-node.herokuapp.com/api/restaurants'
+  host: 'https://restaurant-node.herokuapp.com/api/restaurants',
   //namespace: 'api'
 });
--- app/models/restaurant.js
+++ app/models/restaurant.js
@@ -1,7 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  id: DS.attr(),
   name: DS.attr(),
   city: DS.attr(),
   address: DS.attr(),
